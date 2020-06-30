ce9e8773524ef9234773531923e912ae55af3fe8
ChaconFrancoLuis <francofer7775@gmail.com>
2020-06-30 16:48:51 -0600

    "archivo XML y script XMLparse para laboratorio dia 2"

---
+++ XMLparse.py
@@ -0,0 +1,10 @@
+import xmltodict
+
+#Get el archivo de data XML 
+stream = open('fileXML.xml','r')
+
+#Analizar el formato XML a 'OrderedDict'
+xml = xmltodict.parse(stream.read())
+
+for e in xml["People"]["Person"]:
+    print(e)
